876dab0147b1887b704540428988dd7e75d3bce7
David Griffiths <dgriffiths+gh@gmail.com>
2022-03-19 11:06:12 +0800

    Adding MyModel swift to project

---
+++ github-experiments/mymodel.swift
@@ -0,0 +1,14 @@
+//
+//  mymodel.swift
+//  github-experiments
+//
+//  Created by David Griffiths on 19/3/2022.
+//
+
+import Foundation
+
+// This is an initial test file to demonstrate adding files
+
+struct MyModel {
+    var nbame: String
+}
