6229623b4f0cfe12e0502a68475768e5e504c848
davidiag <101912676+davidiag@users.noreply.github.com>
2022-03-19 11:11:22 +0800

    Update ContentView.swift
    Adding "Editing within GitHub"

--- github-experiments/ContentView.swift
+++ github-experiments/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Code With Chris!")
+        Text("Hello, Code With Chris! Editing within GitHub")
             .padding()
         
     }
