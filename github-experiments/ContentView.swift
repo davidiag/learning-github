9a11d435f97860c31d5fd8757de8fb3cc010ed39
David Griffiths <dgriffiths+gh@gmail.com>
2022-03-19 10:47:24 +0800

    Initial Commit

---
+++ github-experiments/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github-experiments
+//
+//  Created by David Griffiths on 19/3/2022.
+//
+//  Experiments and learning with GitHub
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github-experiments/github_experimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_experimentsApp.swift
+//  github-experiments
+//
+//  Created by David Griffiths on 19/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct github_experimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
